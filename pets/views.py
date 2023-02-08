b949f76d77b172d5bdeed226ab5d0408e46af7eb
wesleydematos <wesleydematos3@gmail.com>
2023-02-08 19:06:26 -0300

    feat: create views and urls from Pets

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response
+from .models import Pet
+from .serializers import PetSerializer
+from groups.models import Group
+from traits.models import Trait
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class PetView(APIView, PageNumberPagination):
+
+    def get(self, request: Request) -> Response:
+        all_pets = Pet.objects.all()
+
+        result_page = self.paginate_queryset(all_pets, request)
+
+        serializer = PetSerializer(result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = PetSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        group_list = serializer.validated_data.pop("group")
+        traits_list = serializer.validated_data.pop("traits")
+        pet_list = serializer.validated_data
+
+        group_obj = Group.objects.filter(
+                scientific_name__iexact=group_list["scientific_name"]
+            ).first()
+
+        if not group_obj:
+            group_obj = Group.objects.create(**group_list)
+
+        pet_obj = Pet.objects.create(**pet_list, group=group_obj)
+
+        for trait in traits_list:
+            trait_obj = Trait.objects.filter(
+                name__iexact=trait["name"]
+            ).first()
+
+            if not trait_obj:
+                trait_obj = Trait.objects.create(**trait)
+
+            pet_obj.traits.add(trait_obj)
+
+        serializer = PetSerializer(pet_obj)
+
+        return Response(serializer.data, 201)
--- traits/serializers.py
+++ traits/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
+
 class TraitSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50)
+    trait_name = serializers.CharField(max_length=50, source="name")
     created_at = serializers.DateTimeField(read_only=True)
